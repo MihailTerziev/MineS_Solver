3fbf1d11b9cb9ea150128b1ebfbc45aed4f5259a
Mihail Terziev <mishoterziev1@gmail.com>
2022-03-03 20:18:25 +0200

    updated solver

--- ms_solver/solver.py
+++ ms_solver/solver.py
@@ -2,24 +2,24 @@ from ms_generate.generator import FieldGenerator
 
 
 class FieldSolver:
-    def count_bombs_around(self, f: FieldGenerator, x_cor: int, y_cor: int) -> int:
+    def count_bombs_around(self, f: FieldGenerator, x_cor: int, y_cor: int):
         counter = 0
 
-        if not x_cor == 0 and f.field[x_cor - 1][y_cor] == '*':
+        if not x_cor == 0 and f.field[x_cor - 1][y_cor] == 'X':
             counter += 1
-        if not x_cor == f.map_size - 1 and f.field[x_cor + 1][y_cor] == '*':
+        if not x_cor == f.map_size - 1 and f.field[x_cor + 1][y_cor] == 'X':
             counter += 1
-        if not y_cor == 0 and f.field[x_cor][y_cor - 1] == '*':
+        if not y_cor == 0 and f.field[x_cor][y_cor - 1] == 'X':
             counter += 1
-        if not y_cor == f.map_size - 1 and f.field[x_cor][y_cor + 1] == '*':
+        if not y_cor == f.map_size - 1 and f.field[x_cor][y_cor + 1] == 'X':
             counter += 1
-        if not x_cor == 0 and not y_cor == size - 1 and f.field[x_cor - 1][y_cor + 1] == '*':
+        if not x_cor == 0 and not y_cor == f.map_size - 1 and f.field[x_cor - 1][y_cor + 1] == 'X':
             counter += 1
-        if not x_cor == 0 and not y_cor == 0 and f.field[x_cor - 1][y_cor - 1] == '*':
+        if not x_cor == 0 and not y_cor == 0 and f.field[x_cor - 1][y_cor - 1] == 'X':
             counter += 1
-        if not x_cor == size - 1 and not y_cor == size - 1 and f.field[x_cor + 1][y_cor + 1] == '*':
+        if not x_cor == f.map_size - 1 and not y_cor == f.map_size - 1 and f.field[x_cor + 1][y_cor + 1] == 'X':
             counter += 1
-        if not x_cor == size - 1 and not y_cor == 0 and f.field[x_cor + 1][y_cor - 1] == '*':
+        if not x_cor == f.map_size - 1 and not y_cor == 0 and f.field[x_cor + 1][y_cor - 1] == 'X':
             counter += 1
 
         return counter
