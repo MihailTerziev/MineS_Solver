fa1050f6612107c6aa1dd48941a68fb16a82fed7
Mihail Terziev <mishoterziev1@gmail.com>
2022-03-04 19:08:12 +0200

    updated generator

--- ms_generate/generator.py
+++ ms_generate/generator.py
@@ -1,6 +1,6 @@
 class FieldGenerator:
     def __init__(self, map_size: int):
-        self.__map_size = map_size
+        self.map_size = map_size
         self.__field = [['0' for _ in range(self.map_size)] for _ in range(self.map_size)]
 
     def __str__(self):
@@ -8,24 +8,6 @@ class FieldGenerator:
         for row in self.__field:
             output += f"{' '.join(row)}\n"
         return output.strip()
-
-    @property
-    def map_size(self):
-        return self.__map_size
-
-    @map_size.setter
-    def map_size(self, map_size):
-        if map_size > 0:
-            self.__set_field(self.__map_size, map_size)
-            self.__map_size = map_size
-
-    def __set_field(self, map_size, new_size):
-        if new_size < map_size:
-            self.__field = [[x for x in row[:new_size]] for row in self.__field[:new_size]]
-        else:
-            diff = new_size - map_size
-            [row.extend(['0' for _ in range(diff)]) for row in self.__field]
-            [self.__field.append(['0' for _ in range(new_size)]) for _ in range(diff)]
 
     def load_mines(self, mine_count: int):
         if mine_count <= 0:
@@ -48,12 +30,27 @@ class FieldGenerator:
             if len(row) == map_size:
                 self.__field.append(row)
 
-    def create_field(self, map_size=0, delimiter=' '):
+    def customize(self, map_size=0, delimiter=' '):
         if not map_size == 0:
             self.__fill(map_size, delimiter)
         else:
             self.__fill(self.map_size, delimiter)
 
-    def save_field(self, path):
+    def resize(self, new_size):
+        if new_size < self.map_size:
+            self.__field = [[x for x in row[:new_size]] for row in self.__field[:new_size]]
+        elif new_size > self.map_size:
+            diff = new_size - self.map_size
+            [row.extend(['0' for _ in range(diff)]) for row in self.__field]
+            [self.__field.append(['0' for _ in range(new_size)]) for _ in range(diff)]
+
+    def save(self, path):
         with open(path, 'w') as file:
             file.write(self.__str__())
+
+
+if __name__ == '__main__':
+    g = FieldGenerator(5)
+    print(g)
+    g.map_size = 7
+    print(g)
