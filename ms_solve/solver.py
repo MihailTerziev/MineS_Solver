9c69f8bd2064132cdfac5f7d8cb61ec931da56bd
Mihail Terziev <mishoterziev1@gmail.com>
2022-03-04 20:24:31 +0200

    updated solver

--- ms_solve/solver.py
+++ ms_solve/solver.py
@@ -3,26 +3,26 @@ from ms_generate.generator import FieldGenerator
 
 class FieldSolver:
     def __init__(self):
-        self.field = []
+        self.__field = []
 
-    def mine_count(self, f: FieldGenerator, x_cor: int, y_cor: int):
+    def mine_count(self, f: FieldGenerator, x_cor: int, y_cor: int, mine_sym: str):
         counter = 0
 
-        if not x_cor == 0 and f.field[x_cor - 1][y_cor] == 'X':
+        if not x_cor == 0 and f.field[x_cor - 1][y_cor] == mine_sym:
             counter += 1
-        if not x_cor == f.map_size - 1 and f.field[x_cor + 1][y_cor] == 'X':
+        if not x_cor == f.map_size - 1 and f.field[x_cor + 1][y_cor] == mine_sym:
             counter += 1
-        if not y_cor == 0 and f.field[x_cor][y_cor - 1] == 'X':
+        if not y_cor == 0 and f.field[x_cor][y_cor - 1] == mine_sym:
             counter += 1
-        if not y_cor == f.__map_size - 1 and f.field[x_cor][y_cor + 1] == 'X':
+        if not y_cor == f.map_size - 1 and f.field[x_cor][y_cor + 1] == mine_sym:
             counter += 1
-        if not x_cor == 0 and not y_cor == f.__map_size - 1 and f.field[x_cor - 1][y_cor + 1] == 'X':
+        if not x_cor == 0 and not y_cor == f.map_size - 1 and f.field[x_cor - 1][y_cor + 1] == mine_sym:
             counter += 1
-        if not x_cor == 0 and not y_cor == 0 and f.field[x_cor - 1][y_cor - 1] == 'X':
+        if not x_cor == 0 and not y_cor == 0 and f.field[x_cor - 1][y_cor - 1] == mine_sym:
             counter += 1
-        if not x_cor == f.__map_size - 1 and not y_cor == f.__map_size - 1 and f.field[x_cor + 1][y_cor + 1] == 'X':
+        if not x_cor == f.map_size - 1 and not y_cor == f.map_size - 1 and f.field[x_cor + 1][y_cor + 1] == mine_sym:
             counter += 1
-        if not x_cor == f.__map_size - 1 and not y_cor == 0 and f.field[x_cor + 1][y_cor - 1] == 'X':
+        if not x_cor == f.map_size - 1 and not y_cor == 0 and f.field[x_cor + 1][y_cor - 1] == mine_sym:
             counter += 1
 
         return counter
@@ -30,4 +30,4 @@ class FieldSolver:
     def load(self, path):
         with open(path, 'r') as file:
             for line in file.readlines():
-                self.field.append([x for x in line.strip()])
+                self.__field.append([x for x in line.strip()])
